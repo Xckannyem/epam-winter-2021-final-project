f375dc462c27966fb25307ba11830c1de5e3716c
Yegor <72563496+yegorks@users.noreply.github.com>
2021-05-03 01:27:05 +0300

    remove flask_bootstrap

--- department_app/__init__.py
+++ department_app/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
-from flask_bootstrap import Bootstrap
 
 from config import Config
 
@@ -14,8 +13,6 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 migrate = Migrate(app, db)
-
-Bootstrap(app)
 
 from department_app import views, models
 
