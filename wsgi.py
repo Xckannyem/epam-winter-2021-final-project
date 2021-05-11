0c51c3b70f4247bc6d050cf0a3ae7204a70a0241
Yegor <72563496+yegorks@users.noreply.github.com>
2021-05-11 22:22:06 +0300

    change host

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
-from department_app import app
+from department_app import create_app
 
+app = create_app()
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
