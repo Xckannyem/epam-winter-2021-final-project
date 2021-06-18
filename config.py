ec8607f43f9e136dcb279ea23b811f1ba1f6323a
Yegor <72563496+yegorks@users.noreply.github.com>
2021-06-18 18:01:22 +0300

    update config

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or \
                  '7b0342f12ee64296aaaa9738c72ca2c4'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-                              'postgresql://user:password@localhost:5432/department_db'
+                              'postgresql://postgres:1234@localhost:5432/department_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
